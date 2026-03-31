70a50134443a029daaa6d50ad22e31504a77db77
Addam Cosmo <addamcosmo2000@gmail.com>
2026-03-31 14:02:32 -0300

    ajustado separação dos arquivos js e css

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const totalPages = 8;
 // CONFIGURAÇÃO DO WEBHOOK
 // =====================================================
 const WEBHOOK_CONFIG = {
-  url: 'https://herculano-n8n.zm9in4.easypanel.host/webhook/relatorio-turno', // Ajuste para sua URL
+  url: 'https://n8n.wellia.cloud/webhook-test/formulario-solicitacao', // Ajuste para sua URL
   method: 'POST',
   headers: {
     'Content-Type': 'application/json'
